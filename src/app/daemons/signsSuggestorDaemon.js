8d575f6d4d391f48041583e91baee9cfa331c49b
Wesnydy Ribeiro <wesnydy@gmail.com>
2019-11-20 18:10:16 -0300

    Changin workspace

--- src/app/daemons/signsSuggestorDaemon.js
+++ src/app/daemons/signsSuggestorDaemon.js
@@ -1,111 +1,24 @@
 import axios from 'axios';
 import env from '../../config/environment';
-import { serverWarning } from '../util/debugger';
+import { serverError } from '../util/debugger';
+import {
+  authenticateOnWikilibras,
+  whoAmIOnWikilibras,
+  postNewTaskOnWikilibras,
+  postNewActionOnWikilibras,
+  resetTaskUserOnWikilibras,
+} from '../util/wikilibrasRequests';
 
 const dictionaryStatsRequest = axios.create({
   baseURL: env.DICTIONARY_STATS_BASE_URL,
   timeout: 10000,
 });
 
-const wikilibrasRequest = axios.create({
-  baseURL: env.WIKILIBRAS_BASE_URL,
-  timeout: 10000,
-});
-
-const authenticateOnWikilibras = async function authenticateOnWikilibrasServer() {
-  try {
-    const response = await wikilibrasRequest({
-      method: 'post',
-      url: env.WIKILIBRAS_AUTHENTICATON_URL,
-      headers: { 'Content-Type': 'application/json' },
-      data: {
-        email: env.WIKILIBRAS_USER_EMAIL,
-        password: env.WIKILIBRAS_PASSWORD,
-      },
-      responseType: 'json',
-      transformResponse: [(data) => `${JSON.parse(data).type} ${JSON.parse(data).token}`],
-    });
-
-    return response.data;
-  } catch (error) {
-    throw new Error(`WikilibrasAuthRequestError: ${error.message}`);
-  }
-};
-
-const whoAmIOnWikilibras = async function whoAmIOnWikilibrasServer() {
-  try {
-    const bearerToken = await authenticateOnWikilibras();
-    const response = await wikilibrasRequest({
-      method: 'get',
-      url: env.WIKILIBRAS_WHOAMI_URL,
-      headers: { Authorization: bearerToken },
-      responseType: 'json',
-      transformResponse: [(data) => JSON.parse(data).user_id],
-    });
-
-    return response.data;
-  } catch (error) {
-    throw new Error(`WikilibrasWhoAmIRequestError: ${error.message}`);
-  }
-};
-
-const postNewActionOnWikilibras = async function postNewActionOnWikilibrasServer(userID, taskID) {
-  try {
-    await wikilibrasRequest({
-      method: 'post',
-      url: env.WIKILIBRAS_NEWACTION_URL,
-      headers: { 'Content-Type': 'application/json' },
-      data: {
-        action_type_id: 1,
-        details: 'no detail',
-        success: true,
-        task_id: taskID,
-        user_id: userID,
-      },
-    });
-
-    await wikilibrasRequest({
-      method: 'put',
-      url: env.WIKILIBRAS_USER_TASK_URL,
-      headers: { 'Content-Type': 'application/json' },
-      data: {
-        current_user_id: null,
-      },
-    });
-  } catch (error) {
-    throw new Error(`WikilibrasNewActionRequestError: ${error.message}`);
-  }
-};
-
-const postNewTaskOnWikilibras = async function postNewTaskOnWikilibrasServer(userID, signID) {
-  try {
-    const response = await wikilibrasRequest({
-      method: 'post',
-      url: env.WIKILIBRAS_NEWTASK_URL,
-      headers: { 'Content-Type': 'application/json' },
-      data: {
-        task_type_id: 1,
-        task_state_id: 1,
-        code: signID,
-        creation_user_id: userID,
-        current_user_id: userID,
-      },
-      responseType: 'json',
-      transformResponse: [(data) => JSON.parse(data).id],
-    });
-
-    await postNewActionOnWikilibras(userID, response.data);
-  } catch (error) {
-    throw new Error(`WikilibrasNewTaskRequestError: ${error.message}`);
-  }
-};
-
 const getMissingSigns = async function getMissingSignsRanking() {
   try {
     const response = await dictionaryStatsRequest({
       method: 'get',
       url: env.DICTIONARY_MISSING_SIGNS_URL,
-      responseType: 'json',
       transformResponse: [(data) => Array.from(JSON.parse(data).signsRanking, (x) => x.name)],
     });
 
@@ -115,22 +28,47 @@ const getMissingSigns = async function getMissingSignsRanking() {
   }
 };
 
+const suggestNewSign = async function suggestNewSignToWikilibras(sign, wikilibrasUserID) {
+  try {
+    const taskID = await postNewTaskOnWikilibras({
+      task_type_id: 1,
+      task_state_id: 1,
+      code: sign,
+      creation_user_id: wikilibrasUserID,
+      current_user_id: wikilibrasUserID,
+    });
+
+    await postNewActionOnWikilibras({
+      action_type_id: 1,
+      details: 'no detail',
+      success: true,
+      task_id: taskID,
+      user_id: wikilibrasUserID,
+    });
+
+    await resetTaskUserOnWikilibras(taskID);
+  } catch (error) {
+    throw new Error(`Failed to suggest '${sign}': ${error.message}`);
+  }
+};
+
 const signsSuggestorDaemon = async function wikilibrasSignsSuggestorDaemon() {
-  setInterval(async () => {
-    try {
-      const signs = await getMissingSigns();
-      const userID = await whoAmIOnWikilibras();
+  // setInterval(async () => {
+  try {
+    const bearerToken = await authenticateOnWikilibras();
+    const wikilibrasUserID = await whoAmIOnWikilibras(bearerToken);
+    const missingSigns = await getMissingSigns();
 
-      const promisesList = [];
-      for (let i = 0; i < signs.length; i += 1) {
-        promisesList.push(postNewTaskOnWikilibras(signs[i], userID));
-      }
-
-      await Promise.all(promisesList);
-    } catch (error) {
-      serverWarning(error.message);
+    const promisesList = [];
+    for (let i = 0; i < missingSigns.length; i += 1) {
+      promisesList.push(suggestNewSign(missingSigns[i], wikilibrasUserID));
     }
-  }, env.SIGNS_SUGGESTION_INTERVAL);
+
+    await Promise.all(promisesList);
+  } catch (error) {
+    serverError(error.message);
+  }
+  // }, env.SIGNS_SUGGESTION_INTERVAL);
 };
 
 export default signsSuggestorDaemon;
---
+++ src/app/util/wikilibrasRequests.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import env from '../../config/environment';
+
+const wikilibrasRequest = axios.create({
+  baseURL: env.WIKILIBRAS_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  timeout: 10000,
+});
+
+const authenticateOnWikilibras = async function authenticateOnWikilibrasServer() {
+  try {
+    const response = await wikilibrasRequest({
+      method: 'post',
+      url: env.WIKILIBRAS_AUTHENTICATON_URL,
+      data: {
+        email: env.WIKILIBRAS_USER_EMAIL,
+        password: env.WIKILIBRAS_PASSWORD,
+      },
+      transformResponse: [(data) => `${JSON.parse(data).type} ${JSON.parse(data).token}`],
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`WikilibrasAuthRequestError: ${error.message}`);
+  }
+};
+
+const whoAmIOnWikilibras = async function whoAmIOnWikilibrasServer(bearerToken) {
+  try {
+    const response = await wikilibrasRequest({
+      method: 'post',
+      url: env.WIKILIBRAS_WHOAMI_URL,
+      headers: { Authorization: bearerToken },
+      transformResponse: [(data) => JSON.parse(data).user_id],
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`WikilibrasWhoAmIRequestError: ${error.message}`);
+  }
+};
+
+const postNewTaskOnWikilibras = async function postNewTaskOnWikilibrasServer(taskData) {
+  try {
+    const response = await wikilibrasRequest.post({
+      method: 'post',
+      url: env.WIKILIBRAS_NEWTASK_URL,
+      data: taskData,
+      transformResponse: [(data) => JSON.parse(data).id],
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`WikilibrasNewTaskRequestError: ${error.message}`);
+  }
+};
+
+const postNewActionOnWikilibras = async function postNewActionOnWikilibrasServer(actionData) {
+  try {
+    await wikilibrasRequest.post(env.WIKILIBRAS_NEWACTION_URL, { actionData });
+  } catch (error) {
+    throw new Error(`WikilibrasNewActionRequestError: ${error.message}`);
+  }
+};
+
+const resetTaskUserOnWikilibras = async function resetTaskUserOnWikilibrasServer(taskID) {
+  try {
+    const resetTaskUserURL = new URL(taskID, env.WIKILIBRAS_TASK_USER_URL);
+    await wikilibrasRequest.put(resetTaskUserURL, { current_user_id: null });
+  } catch (error) {
+    throw new Error(`WikilibrasResetTaskUserRequestError: ${error.message}`);
+  }
+};
+
+export {
+  authenticateOnWikilibras,
+  whoAmIOnWikilibras,
+  postNewTaskOnWikilibras,
+  postNewActionOnWikilibras,
+  resetTaskUserOnWikilibras,
+};
