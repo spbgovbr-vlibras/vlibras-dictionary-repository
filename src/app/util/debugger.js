7e783bedf12564be20e3e0c53f24a02afb37c040
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2020-07-01 14:58:58 -0300

    apifolder structure

--- src/app/util/debugger.js
+++
@@ -1,8 +0,0 @@
-import debug from 'debug';
-
-export const serverInfo = debug('vlibras-dictionary-repository:info');
-export const serverWarning = debug('vlibras-dictionary-repository:warning');
-export const serverError = debug('vlibras-dictionary-repository:error');
-export const databaseError = debug('vlibras-dictionary-db:error');
-export const daemonInfo = debug('vlibras-dictionary-daemon:info');
-export const daemonError = debug('vlibras-dictionary-daemon:error');
