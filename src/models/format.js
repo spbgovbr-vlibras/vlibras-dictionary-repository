fe212870a031bbf4146b56792ba29f2f83358da9
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2020-07-07 19:04:23 -0300

    adding missing model

---
+++ src/models/format.js
@@ -0,0 +1,29 @@
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  class Format extends Model {
+    associate(models) {
+      this.belongsToMany(models.File, { through: 'FileFormats' });
+    }
+  }
+
+  Format.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    format: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  }, {
+    sequelize,
+    modelName: 'Format',
+    timestamps: true,
+  });
+
+  return Format;
+};
