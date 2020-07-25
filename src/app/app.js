d2ac4ad578bd7c5bcf9dfa806eb72a6d167379b3
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2020-07-25 16:21:52 -0300

    fixing logger format import

--- src/app/app.js
+++ src/app/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(compression());
 app.use(helmet());
-app.use(logger(config.server.logger));
+app.use(logger(config.server.logger.format));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
