6238094568bb8669ed5a1917cb01516ab7adecf8
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2020-08-11 08:28:36 -0300

    adding format constants

---
+++ src/lib/constants/formats.js
@@ -0,0 +1,5 @@
+export default Object.freeze({
+  BLEND: 'BLEND',
+  BUNDLE: 'BUNDLE',
+  VIDEO: 'MPEG-4',
+});
--- src/lib/constants/index.js
+++ src/lib/constants/index.js
@@ -1,11 +1,13 @@
 import VERSIONS from './versions';
 import PLATFORMS from './platforms';
 import REGIONS from './regions';
+import FORMATS from './formats';
 import EXTENSIONS from './extensions';
 
 export default Object.freeze({
   VERSIONS,
   PLATFORMS,
   REGIONS,
+  FORMATS,
   EXTENSIONS,
 });
