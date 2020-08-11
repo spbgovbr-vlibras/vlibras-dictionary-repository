318e54cb5b787d52783c519555fa810f3712e8f9
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2020-08-11 12:19:27 -0300

    method to register sign source files

--- src/services/SignService.js
+++ src/services/SignService.js
@@ -1,16 +1,21 @@
-import { UniqueConstraintError } from 'sequelize';
+import { Op } from 'sequelize';
+
 import models from '../models';
+import constants from '../lib/constants';
 
 export default class SignService {
-  static async registerNewSign(signName, signVersion, signRegion = 'BR') {
+  static async registerNewSignBundle(signName, signVersion, signRegion = 'BR') {
     try {
       const [version, platforms, region, format] = await Promise.all([
         models.Version.findOne({ where: { version: signVersion } }),
-        models.Platform.findAll(),
+        models.Platform.findAll({
+          where: { platform: { [Op.not]: constants.PLATFORMS.WIKILIBRAS } },
+        }),
         models.Region.findOne({ where: { region: signRegion } }),
-        models.Format.findOne({ where: { format: 'BUNDLE' } }),
+        models.Format.findOne({ where: { format: constants.FORMATS.BUNDLE } }),
       ]); // do not change the sequence
 
+      // TODO: include sign metadata when create
       const sign = await models.Sign.create({ name: signName });
 
       await Promise.all([
@@ -25,10 +30,52 @@ export default class SignService {
       return sign.get({ plain: true });
     } catch (registerNewSignError) {
       console.error(registerNewSignError); // TODO: change to logger
-      if (registerNewSignError instanceof UniqueConstraintError) {
-        throw new Error('sign already registered in the database');
+      throw new Error('an unexpected error occurred while registering sign bundle');
+    }
+  }
+
+  static async registerNewSignSource(signName, signVersion, signFormat, signRegion = 'BR') {
+    try {
+      const sign = await models.Sign.findOne({
+        where: {
+          name: signName,
+          '$Versions.version$': signVersion,
+          '$Regions.region$': signRegion,
+        },
+        include: [
+          { model: models.Version, as: 'Versions' },
+          { model: models.Platform, attributes: ['platform'] },
+          { model: models.Region, as: 'Regions' },
+          { model: models.Format, attributes: ['format'] },
+        ],
+      });
+
+      if (sign === null) {
+        return null;
       }
-      throw new Error('an unexpected error occurred while registering sign');
+
+      const { Platforms, Formats } = sign.get({ plain: true });
+      if (
+        Platforms.some((platform) => platform.platform === constants.PLATFORMS.WIKILIBRAS)
+        && Formats.some((format) => format.format === signFormat)
+      ) {
+        throw new Error('sign source already registered in the database');
+      }
+
+      const [platform, format] = await Promise.all([
+        models.Platform.findOne({ where: { platform: constants.PLATFORMS.WIKILIBRAS } }),
+        models.Format.findOne({ where: { format: signFormat } }),
+      ]); // do not change the sequence
+
+      await Promise.all([
+        sign.addFormat(format),
+        sign.addPlatform(platform),
+      ]);
+
+      return sign.get({ plain: true });
+    } catch (registerNewSignSourceError) {
+      console.error(registerNewSignSourceError); // TODO: change to logger
+      throw new Error('an unexpected error occurred while registering sign source');
     }
   }
 
@@ -50,11 +97,11 @@ export default class SignService {
         ],
       });
 
-      if (sign !== null) {
-        return sign.get({ plain: true });
+      if (sign === null) {
+        return null;
       }
 
-      return null;
+      return sign.get({ plain: true });
     } catch (getRegisteredSignError) {
       console.error(getRegisteredSignError); // TODO: change to logger
       throw new Error('an unexpected error occurred while retrieving sign record');
